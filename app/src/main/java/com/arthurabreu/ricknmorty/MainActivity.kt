2d8b0f5bf0969b06defa17ae8344b35482dacd5c
Arthur Abreu <arthurabreu@Arthurs-MacBook-Pro.local>
2023-08-07 17:21:31 -0300

    Initial commit

---
+++ app/src/main/java/com/arthurabreu/ricknmorty/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.arthurabreu.ricknmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.arthurabreu.ricknmorty.ui.theme.RickAndMortyGraphQLTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyGraphQLTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
