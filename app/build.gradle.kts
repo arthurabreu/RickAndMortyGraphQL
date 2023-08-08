e832b4861f8c87e7bb05d681221260ff1aba89d5
Arthur Abreu <arthurabreupro@gmail.com>
2023-08-08 13:47:51 -0300

    Added a Unit Test for the Apollo Client

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation("com.apollographql.apollo3:apollo-runtime:4.0.0-alpha.2")
 
     // Tests
+    testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/test/java/com/arthurabreu/ricknmorty/domain/ApolloRMClientTest.kt
@@ -0,0 +1,46 @@
+package com.arthurabreu.ricknmorty.domain
+
+import com.arthurabreu.ricknmorty.data.UICharacters
+import com.arthurabreu.ricknmorty.data.UILocation
+import com.arthurabreu.ricknmorty.domain.usecases.GetCharactersUseCase
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ApolloRMClientTest {
+
+    /*
+     * In this test, we use the coEvery function from MockK to set up a co-routine stub for the
+     * getCharacters method. This allows us to control what the method returns when it's called,
+     *  and then we can verify that the use case properly calls this method and returns the expected
+     * result.
+     */
+    @Test
+    fun `execute should return characters from client`() = runBlocking {
+        // Arrange
+        val expectedCharacters = listOf(
+            UICharacters(
+                name = "Luke Skywalker",
+                id = "1",
+                image = "https://via.placeholder.com/150",
+                status = "Alive",
+                species = "Human",
+                location = UILocation(name = "Tatooine", id = "2"),
+                gender = "Male"
+            )
+        )
+
+        val client: RMClient = mockk()
+        coEvery { client.getCharacters() } returns expectedCharacters
+
+        val useCase = GetCharactersUseCase(client)
+
+        // Act
+        val actualCharacters = useCase.execute()
+
+        // Assert
+        assertEquals(expectedCharacters, actualCharacters)
+    }
+}
